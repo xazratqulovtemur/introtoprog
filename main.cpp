b4d8736871655bf9f79a6379570831b8b4e93bad
xazratqulovtemur <khazratkulovtemur@gmail.com>
2025-02-06 13:20:48 +0500

    Lab2

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main() {
+    //problem1
+    /* cout<<"What is your name?"<<endl;
+     string name;
+     cin>>name;
+     cout<<"Cool, "<<name<<setw(5)<<" How old are you?"<<endl;
+     int age;
+     cin>>age;
+     cout<<"That is great!"<<setw(5)<<name<<" you are "<<age<<" years old";*/
+
+    //problem2
+    /*int num;
+    for (int i=1; i<=5; i++)
+        cout<<i<<setw(5);*/
+    //problem3
+    /*int a,b;
+    cin>>a>>b;
+    int me=a;
+    a=b;
+    b=me;
+    cout<<"a="<<a<<" and b="<<b<<endl;*/
+    //problem5
+    /*string name;
+    cin>>name;
+    const int n=name.length();
+    cout<<setw((100-n)/2)<<name<<setw((100/n)/2);
+
+    */
+    return 0;
+}
